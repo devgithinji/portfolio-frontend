025cb0df90b490d2fb3683c6868dce73492273a7
dennis githinji <wakahiad@gmail.com>
2023-01-03 00:08:13 +0300

    form items responsive

--- components/admin/blog/AddArticleForm.js
+++ components/admin/blog/AddArticleForm.js
@@ -23,19 +23,23 @@ const AddArticleForm = () => {
     return (
         <div className="admin-section card">
             <div className="add-article-form">
-                <FormInput value={title} setValue={setTitle} placeholder='Title' id='title' name='title'
-                           error={errors.name}/>
-                <SelectInput type="select" value={tags} setValue={setTags} id='category' options={options}
-                             name='Category' error={errors.category} multiselect={false}/>
-                <div className="form-item admin-form-item">
-                    <label htmlFor="content">Article Content</label>
-                    <MdEditor style={{height: '500px'}} renderHTML={text => mdParser.render(text)}
-                              onChange={handleEditorChange}/>
-                </div>
-                <button type="submit" className="form-btn"
-                        disabled={isLoading}>
-                    {isLoading ? 'Please wait...' : 'Add Article'}
-                </button>
+                <form className="form-container" action="">
+                    <div className="inputs-container">
+                        <FormInput value={title} setValue={setTitle} placeholder='Title' id='title' name='title'
+                                   error={errors.name}/>
+                        <SelectInput type="select" value={tags} setValue={setTags} id='category' options={options}
+                                     name='Category' error={errors.category} multiselect={false}/>
+                        <div className="form-item admin-form-item blog-input">
+                            <label htmlFor="content">Article Content</label>
+                            <MdEditor style={{height: '500px'}} renderHTML={text => mdParser.render(text)}
+                                      onChange={handleEditorChange}/>
+                        </div>
+                    </div>
+                    <button type="submit" className="form-btn"
+                            disabled={isLoading}>
+                        {isLoading ? 'Please wait...' : 'Add Article'}
+                    </button>
+                </form>
             </div>
         </div>
     );
--- components/admin/projects/AddProjectForm.js
+++ components/admin/projects/AddProjectForm.js
@@ -10,7 +10,7 @@ const AddProjectForm = () => {
     const [repoLink, setRepoLink] = useState('');
     const [tags, setTags] = useState([]);
     const [image, setImage] = useState('')
-    const [existingFile, setExistingFile] = useState({url:''})
+    const [existingFile, setExistingFile] = useState({url: ''})
     const [errors, setErrors] = useState({})
 
     const [isLoading, setLoading] = useState(false);
@@ -18,23 +18,28 @@ const AddProjectForm = () => {
     const options = ["option one", "option two", "option three", "option four"]
     return (
         <div className="admin-section card">
-            <div className="add-project-form">
-                <FormInput value={name} setValue={setName} placeholder='Name' id='name' name='Name'
-                           error={errors.name}/>
-                <FormInput value={siteLink} setValue={setLink} placeholder='Site Link' id='site-link' name='Site Link'
-                           error={errors.site_link}/>
-                <FormInput value={repoLink} setValue={setRepoLink} placeholder='Repo Link' id='repo-link'
-                           name='Repo Link'
-                           error={errors.repo_link}/>
-                <SelectInput type="select" value={tags} setValue={setTags} id='category' options={options} name='Category'
-                           error={errors.category} multiselect={true}/>
-                <FileInput name="Image" setValue={setImage} value={image} type="file" id="image" error={errors.image}
-                           existingFile={existingFile}/>
-                <FormInput type='textarea' value={description} setValue={setDescription} placeholder='Description'
-                           id='description' name='Description' error={errors.description}/>
+            <form className="form-container" action="">
+                <div className="inputs-container">
+                    <FormInput value={name} setValue={setName} placeholder='Name' id='name' name='Name'
+                               error={errors.name}/>
+                    <FormInput value={siteLink} setValue={setLink} placeholder='Site Link' id='site-link'
+                               name='Site Link'
+                               error={errors.site_link}/>
+                    <FormInput value={repoLink} setValue={setRepoLink} placeholder='Repo Link' id='repo-link'
+                               name='Repo Link'
+                               error={errors.repo_link}/>
+                    <SelectInput type="select" value={tags} setValue={setTags} id='category' options={options}
+                                 name='Category'
+                                 error={errors.category} multiselect={true}/>
+                    <FileInput name="Image" setValue={setImage} value={image} type="file" id="image"
+                               error={errors.image}
+                               existingFile={existingFile}/>
+                    <FormInput type='textarea' value={description} setValue={setDescription} placeholder='Description'
+                               id='description' name='Description' error={errors.description}/>
+                </div>
                 <button type="submit" className="form-btn"
                         disabled={isLoading}>{isLoading ? 'Please wait..' : 'Add Project'}</button>
-            </div>
+            </form>
         </div>
     );
 };
