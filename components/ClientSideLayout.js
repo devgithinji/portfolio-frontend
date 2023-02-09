160a621dc817973ec7ac9527a31ada7189d6960d
dennis githinji <wakahiad@gmail.com>
2023-02-10 00:44:20 +0300

    google analytics  fix

--- components/ClientSideLayout.js
+++ components/ClientSideLayout.js
@@ -10,19 +10,18 @@ const ClientSideLayout = ({children}) => {
     }, [])
     return (
         <>
-            <Header>
-                <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-                        strategy="afterInteractive"/>
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}/>
+            <Script id="google-analytics" strategy="lazyOnload">
+                {`
                       window.dataLayer = window.dataLayer || [];
                       function gtag(){window.dataLayer.push(arguments);}
                       gtag('js', new Date());
             
-                      gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}',{
+                      page_path: window.location.pathname,
+                      });
                     `}
-                </Script>
-            </Header>
+            </Script>
             <Header/>
             <main>
                 {children}
