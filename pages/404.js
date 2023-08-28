b54cf7d871e7860eb502d214b5c7070daa2ff7aa
dennis githinji <wakahiad@gmail.com>
2023-08-28 11:32:12 +0300

    error pages fix

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Error from "../components/error/Error";
+import Head from "next/head";
 
 const NotFoundPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Dennis Githinji | Page Not Found</title>
+                <meta name="description" content="Page not Found"/>
+                <meta name="author" content="Dennis Githinji"/>
+                <meta name="keywords" content="Dennis, Githinji, Software Engineer, Java, JavaScript, React, Node Js"/>
+                <link rel="icon" type="image/x-icon" href="/images/dennis-githinji.png"/>
+            </Head>
 
-    return <Error message='page not found'/>;
+            <Error message='page not found'/>
+        </>
+    )
 };
 
 export default NotFoundPage;
--- pages/500.js
+++ pages/500.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Error from "../components/error/Error";
+import Head from "next/head";
 
 const NotFoundPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Dennis Githinji | Error</title>
+                <meta name="description" content="Something went wrong"/>
+                <meta name="author" content="Dennis Githinji"/>
+                <meta name="keywords" content="Dennis, Githinji, Software Engineer, Java, JavaScript, React, Node Js"/>
+                <link rel="icon" type="image/x-icon" href="/images/dennis-githinji.png"/>
+            </Head>
 
-    return <Error message='something went wrong!'/>;
+            <Error message='something went wrong!'/>
+        </>
+    )
 };
 
 export default NotFoundPage;
